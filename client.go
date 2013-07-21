e08032d6bbbd8c3506ed9eb699ad49fe10f0489e
Alex Suraci <suraci.alex@gmail.com>
2013-07-21 01:46:27 -0700

    add client
    wraps connection, handling reconnects and implicitly opening new connections
    for requests that would block others (i.e. stream, run, link)

---
+++ client.go
@@ -0,0 +1,74 @@
+package warden
+
+import (
+	"time"
+)
+
+type Client struct {
+	SocketPath string
+
+	connection chan *Connection
+}
+
+func NewClient(socketPath string) *Client {
+	return &Client{
+		SocketPath: socketPath,
+		connection: make(chan *Connection),
+	}
+}
+
+func (c *Client) Connect() error {
+	conn, err := Connect(c.SocketPath)
+	if err != nil {
+		return err
+	}
+
+	go c.serveConnections(conn)
+
+	return nil
+}
+
+func (c *Client) Create() (*CreateResponse, error) {
+	return (<-c.connection).Create()
+}
+
+func (c *Client) Destroy(handle string) (*DestroyResponse, error) {
+	return (<-c.connection).Destroy(handle)
+}
+
+func (c *Client) Spawn(handle, script string) (*SpawnResponse, error) {
+	return (<-c.connection).Spawn(handle, script)
+}
+
+func (c *Client) Stream(handle string, jobId uint32) (chan *StreamResponse, error) {
+	return c.acquireConnection().Stream(handle, jobId)
+}
+
+func (c *Client) Run(handle, script string) (*RunResponse, error) {
+	return c.acquireConnection().Run(handle, script)
+}
+
+func (c *Client) serveConnections(conn *Connection) {
+	for stop := false; !stop; {
+		select {
+		case <-conn.disconnected:
+			stop = true
+			break
+
+		case c.connection <- conn:
+		}
+	}
+
+	go c.serveConnections(c.acquireConnection())
+}
+
+func (c *Client) acquireConnection() *Connection {
+	for {
+		conn, err := Connect(c.SocketPath)
+		if err == nil {
+			return conn
+		}
+
+		time.Sleep(500 * time.Millisecond)
+	}
+}
